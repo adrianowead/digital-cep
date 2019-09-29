63b5624635c0c1e0f2020f672dcfd235dd9834ff
Adriano Maciel <adriano_mail@hotmail.com>
2019-09-29 04:38:54 -0300

    Add PHPUnit

--- src/Search.php
+++ src/Search.php
@@ -8,8 +8,22 @@ class Search{
     public function getAddressFromZipcode(string $zipCode): array{
         $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
 
-        $get = file_get_contents($this->url . $zipCode . "/json");
+        $get = $this->getFromServer($zipCode);
 
         return (array) json_decode($get);
+    }
+
+    private function getFromServer($zipCode){
+        $get = file_get_contents($this->url . $zipCode . "/json");
+
+        return $get;
+    }
+
+    private function processData($data){
+        foreach( $data as $k => $v ){
+            $data[$k] = trim($v);
+        }
+
+        return $data;
     }
 }
---
+++ tests/SearchTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Wead\DigitalCep\Search;
+
+class SearchTest extends TestCase{
+
+    /**
+     * @dataProvider dadosTeste
+     */
+    public function testGetAddressFromZipcodeDefaultUsage(string $input, array $esperado){
+        $resultado = new Search;
+        $resultado = $resultado->getAddressFromZipcode($input);
+
+        $this->assertEquals($esperado, $resultado);
+    }
+
+    public function dadosTeste(){
+        return [
+            "Endereço Paraça da Sé" => [
+                "01001000",
+                [
+                    "cep" => "01001-000",
+                    "logradouro" => "Praça da Sé",
+                    "complemento" => "lado ímpar",
+                    "bairro" => "Sé",
+                    "localidade" => "São Paulo",
+                    "uf" => "SP",
+                    "unidade" => "",
+                    "ibge" => "3550308",
+                    "gia" => "1004"
+                ]
+            ],
+            "Endereço Qualquer" => [
+                "03624010",
+                [
+                    "cep" => "03624-010",
+                    "logradouro" => "Rua Luís Asson",
+                    "complemento" => "",
+                    "bairro" => "Vila Buenos Aires",
+                    "localidade" => "São Paulo",
+                    "uf" => "SP",
+                    "unidade" => "",
+                    "ibge" => "3550308",
+                    "gia" => "1004"
+                ]
+            ]
+        ];
+    }
+}
